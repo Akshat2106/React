4b4c48ee968539e865fe685222f8210dcb3f5a5d
Akshat2106 <akshatpatidar2106@gmail.com>
2023-02-06 13:30:02 +0530

    react 32

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+function ExpenseItem(){
+    return(
+        <div>
+            <div><h1>Expense Items</h1></div>
+        <div>
+        Food Rs 10
+<br></br>
+Petrol Rs 100
+<br></br>
+Movies Rs 200
+        </div>
+        </div>
+    )
+};
+
+export default ExpenseItem;
