401ad772975d916682f3ee4af005eb393148dc0b
Akshat2106 <akshatpatidar2106@gmail.com>
2023-02-07 14:47:51 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import ExpenseItem from './components/ExpenseItem';
-//import logo from './logo.svg';
-//import './App.css';
 
-function App() {
+function App(){
+  const expenses=[
+    {title:'car insurence', amount : 454545, LocationOfExpenditure:'jaipur'},
+    {title:'food', amount:6325,  LocationOfExpenditure:'indore'},
+    {title:'entertainment', amount: 5442, LocationOfExpenditure:'jaisalmer'},
+    {title:'travelling', amount:25635,LocationOfExpenditure:'kota'},
+    {title:'rent', amount:12154,  LocationOfExpenditure:'bundi'}
+  ];
   return (
-  <div className="App">
-  <header>
-  <ExpenseItem></ExpenseItem>
-  </header>
-    </div>
-  )
-};
+  <div>
+    <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} Location={expenses[0].LocationOfExpenditure}></ExpenseItem>
+  <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} Location={expenses[1].LocationOfExpenditure}></ExpenseItem>
+  <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} Location={expenses[2].LocationOfExpenditure}></ExpenseItem>
+  <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} Location={expenses[3].LocationOfExpenditure}></ExpenseItem>
+  <ExpenseItem title={expenses[4].title} amount={expenses[4].amount} date={expenses[4].date} Location={expenses[4].LocationOfExpenditure}></ExpenseItem>
+    
+
+     </div>
+  );
+}
+  
+
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,16 @@
 import './ExpenseItem.css';
-function ExpenseItem(){
-    const expenseDate= new Date(2023,2,6);
-    const LocationOfExpenditure= 'jaipur';
-    return(
+function ExpenseItem(props){
+return (
         <div className="expense-item">
-            <div><h1>Expense Items</h1></div>
-        <div className="expense-item__descriptio">
-        Food Rs 10
-     <br></br>
-        Petrol Rs 100
-<br></br>
-Movies Rs 200
-<div>{LocationOfExpenditure}</div>
+        
+         <div><h1>{props.title}</h1></div>
+        <h4>{props.Location}</h4>
+       
+        <h4>{props.title}</h4>
+        <div className='expense-item__price'>{props.amount}</div>
+        
         </div>
-        </div>
+       
     )
 };
-
 export default ExpenseItem;
